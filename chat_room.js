f70a26ccdac31a1b0b714a65624e44ee65892536
Shreyansh-Banger <106863880+Shreyansh-Banger@users.noreply.github.com>
2022-10-15 16:28:34 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCY_8DiBBIn5Z7SoFms5YswD2kkXuJLceU",
+    authDomain: "sb-chat-dfcc1.firebaseapp.com",
+    databaseURL: "https://sb-chat-dfcc1-default-rtdb.firebaseio.com",
+    projectId: "sb-chat-dfcc1",
+    storageBucket: "sb-chat-dfcc1.appspot.com",
+    messagingSenderId: "107672699779",
+    appId: "1:107672699779:web:b6ae738cb76549d1eb2cb6"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+function logout()
+{
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "index.html";
+    
+}
